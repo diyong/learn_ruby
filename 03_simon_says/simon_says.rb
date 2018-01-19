b4dd4532f3e28210a9d3d6b990c37b354b7cbc15
diyong <diyong@sandiego.edu>
2018-01-19 15:58:20 -0800

    Complete simon_says exercise

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
-#write your code here
+def echo *args
+	return "#{args.join(" ")}"
+end
+
+def shout *args
+	return "#{args.join(" ").upcase}"
+end
+
+def repeat string, x=2
+	return (string.split() * x).join(" ")
+end
+
+def start_of_word string, x
+	return string.slice(0, x)
+end
+
+def first_word string
+	string = string.split(" ")
+	return string[0]
+end
+
+def titleize title
+	title = title.split(" ")
+
+	title.map! do |e|
+		if title.index(e) == 0
+			e.capitalize
+		elsif e != "the" && e != "over" && e != "and"
+			e.capitalize
+		else 
+			e.downcase
+		end
+	end
+
+	return title.join(" ")
+end
