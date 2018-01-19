c85507488ed0a10d13be6921dcecbc819e96714d
diyong <diyong@sandiego.edu>
2018-01-18 22:37:31 -0800

    Finish first three exercises

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,7 @@
-#write your code here
+def ftoc num
+	return (num - 32.0) * (5.0 / 9.0)
+end
+
+def ctof num
+	return (num * (9.0 / 5.0)) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
-#write your code here
+def add numx, numy
+	return numx.to_f + numy.to_f
+end
+
+def subtract numx, numy
+	return numx.to_f - numy.to_f
+end
+
+def sum array
+	return array.sum
+end
+
+def multiply *args
+	return args.reject(&:zero?).reduce(:*)
+end
+
+def power x, y
+	return x ** y
+end
+
+def factorial x
+	result = 1
+
+	for i in 1..x do
+		result *= i
+	end
+
+	return result
+end
