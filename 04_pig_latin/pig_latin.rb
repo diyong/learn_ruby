0303340a0d5a7a550c68fa97159f1d652277444b
diyong <diyong@sandiego.edu>
2018-01-19 16:59:50 -0800

    Complete pig_latin exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
-#write your code here
+def translate phrase
+	phrase = phrase.split(" ")
+
+	phrase.map! do |e|
+		i = 0
+
+		while i < e.length
+			if e.match?(/\bqu/i)
+				e = e.slice(2..-1) + e.slice(0..1)
+			end
+
+			if !e[i].match?(/[aeiou]/i)
+				e = e.slice(1..-1) + e.slice(0)
+			else
+				break
+			end
+		end
+		e + "ay"
+	end
+
+	return phrase.join(" ")
+end
