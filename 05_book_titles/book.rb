7d1bf63898046768f6847778a7e15f7811a272fb
diyong <diyong@sandiego.edu>
2018-01-19 18:03:16 -0800

    Complete book_titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title=(phrase)
+		phrase = phrase.split(" ")
+		l_case_words = Regexp.union(/^the$/, /^an$/, /^and$/, /^a$/, /^in$/, /^of$/)
+
+		phrase.map! do |e|
+			if phrase.index(e) == 0
+				e.capitalize
+			elsif phrase.index(e) != 0 and !e.match?(l_case_words)
+				e.capitalize
+			else
+				e.downcase
+			end
+		end
+
+		@title = phrase.join(" ")
+	end
+
+
 end
